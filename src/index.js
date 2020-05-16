e38df1341bc1b15af31b0365b42792f48baaaeeb
github username <github email address>
2020-05-16 07:29:35 -0400

    create store, middleware, compose, combinReducers, thunk, Provider

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+
+const users = () => []
+
+const reducer = combineReducers({ 
+  users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
