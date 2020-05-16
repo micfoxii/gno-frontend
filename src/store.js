92bf5e0a90627de19d0276991446b711ee287683
github username <github email address>
2020-05-16 14:02:48 -0400

    current user reducer and store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import thunk from 'redux-thunk'
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
 import usersReducer from './reducers/users/users.js'
+import currentUser from './reducers/users/currentUser.js'
 
 const reducer = combineReducers({ 
-  users: usersReducer
+  users: usersReducer,
+  currentUser
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
