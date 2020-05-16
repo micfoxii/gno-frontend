c5acfea7c91dd1fa550f418646778ee73221ac38
github username <github email address>
2020-05-16 18:30:14 -0400

    store and app

--- src/store.js
+++ src/store.js
@@ -5,11 +5,14 @@ import currentUser from './reducers/users/currentUser.js'
 import locationsReducer from './reducers/locations/locations.js'
 import currentLocation from './reducers/locations/currentLocation.js'
 
+import loginForm from './reducers/users/loginForm.js'
+
 const reducer = combineReducers({ 
   users: usersReducer,
   currentUser,
   locations: locationsReducer,
-  currentLocation
+  currentLocation,
+  loginForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
