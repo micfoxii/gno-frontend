71eadadb3bad18ac1eb301cf758fbb25aa1bc100
github username <github email address>
2020-05-18 02:21:46 -0400

    get current user for sessions

--- src/actions/users/currentUser.js
+++ src/actions/users/currentUser.js
@@ -31,13 +31,32 @@ export const login = credentials => {
             if (user.error) {
                 alert(user.error)
             } else {
-                dispatch(setCurrentUser())
+                dispatch(setCurrentUser(user))
             }
         })
         .catch(console.log)
     }
 }
 
-export const getCurrentUser = () => {
-
+export const getCurrentUser = credentials => {
+    console.log("login credentials: ", credentials)
+    return dispatch => {
+        return fetch("http://localhost:3001/login", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(resp => resp.json())
+        .then( user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
 }
