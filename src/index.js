ec6cd334b0b59d6e3c6e71fb6a7a6ea71f25a5c2
github username <github email address>
2020-05-19 13:34:22 -0400

    Browser Router

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import * as serviceWorker from './serviceWorker';
 
 import {Provider} from 'react-redux'
 import store from './store.js'
-
+import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
     
   </React.StrictMode>,
